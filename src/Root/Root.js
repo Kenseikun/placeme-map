776dece367f84b7b062344aa3f4b99bc350ad18d
Sebastian Dziechciarz <sebastian.dziechciarz@wp.pl>
2021-07-14 10:17:05 +0200

    [WIP] Implement Leaflet Map

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Root.scss";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Root = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [position, setPosition] = useState([52.229676, 21.012229]);
 
-export default Root
+  return (
+    <div className="map__container">
+      <MapContainer center={position} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={position}>
+          <Popup>Hello :D</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Root;
