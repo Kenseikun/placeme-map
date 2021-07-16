864813c886297d478a0a8d3aa09f1d9d282cfccb
Sebastian Dziechciarz <sebastian.dziechciarz@wp.pl>
2021-07-16 13:25:15 +0200

    [WIP] Reformat code

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,24 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Root.scss";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { CircularProgress } from "@material-ui/core";
+import Form from "../components/Form";
+import Map from "../components/Map";
 
 const Root = () => {
-  const [position, setPosition] = useState([52.229676, 21.012229]);
+  const [position, setPosition] = useState([51.505, -0.09]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const latitudeValidation =
+    "^(\\+|-)?((\\d((\\.)|\\.\\d{1,6})?)|(0*?[0-8]\\d((\\.)|\\.\\d{1,6})?)|(0*?90((\\.)|\\.0{1,6})?))$";
+  const longitudeValidation =
+    "^(\\+|-)?((\\d((\\.)|\\.\\d{1,6})?)|(0*?\\d\\d((\\.)|\\.\\d{1,6})?)|(0*?1[0-7]\\d((\\.)|\\.\\d{1,6})?)|(0*?180((\\.)|\\.0{1,6})?))$";
+
+  const getCoordinates = () => {
+    axios
+      .get(`https://devcube.placeme.pl/api/getGeoJSON?lat=51.12&lng=20.12`)
+      .then((response) => {
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getCoordinates();
+  }, []);
+
+  const setCoordinates = (e) => {
+    e.preventDefault();
+    const latitude = +e.target.lat.value;
+    const longitude = +e.target.lon.value;
+
+    setPosition([latitude, longitude]);
+
+    e.target.reset();
+  };
+
 
   return (
-    <div className="map__container">
-      <MapContainer center={position} zoom={13}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        <Marker position={position}>
-          <Popup>Hello :D</Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <>
+      {!isLoading ? (
+        <>
+          <Form setCoordinates={setCoordinates} />
+          <Map position={position} />
+        </>
+      ) : (
+        <>
+          <CircularProgress color="secondary" />
+        </>
+      )}
+    </>
   );
 };
 
